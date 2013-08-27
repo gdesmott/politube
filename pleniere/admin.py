bd18b3a7d0c1edaa1087f5a88ee20605c95e4bbd
Guillaume Desmottes <guillaume.desmottes@collabora.co.uk>
2013-08-27 16:00:45 +0200

    add pleniere/models.py

---
+++ pleniere/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from pleniere.models import Pleniere, AgendaItem
+
+admin.site.register(Pleniere)
+admin.site.register(AgendaItem)
--- pleniere/models.py
+++ pleniere/models.py
@@ -1,3 +1,32 @@
+import datetime
+
 from django.db import models
 
-# Create your models here.
+VIDEOS = 'http://urlab-srv0.ulb.ac.be/~cassidy/'
+
+class Pleniere(models.Model):
+    chambre_id = models.CharField(max_length=200)
+    source = models.URLField()
+    date = models.DateTimeField('date')
+    title = models.CharField(max_length=200)
+    webm = models.CharField(max_length=200)
+
+    def __unicode__(self):
+        return "%s - %s" % (self.chambre_id, self.title)
+
+    def getWebmStream(self):
+        return '%s/webm/%s' % (VIDEOS, self.webm)
+
+class AgendaItem(models.Model):
+    pleniere = models.ForeignKey(Pleniere)
+    # time in seconds
+    time = models.IntegerField()
+    speaker = models.CharField(max_length=200)
+    section = models.CharField(max_length=200, null=True)
+    subsection = models.CharField(max_length=200, null=True)
+
+    def __unicode__(self):
+        return "%s - %s (%s)" % (self.speaker, self.pleniere.title, self.time)
+
+    def displayTime(self):
+        return str(datetime.timedelta(seconds=self.time))
