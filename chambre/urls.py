88da6c2964c6191ac2adc7fbd616de7d59ff75ec
Guillaume Desmottes <guillaume.desmottes@collabora.co.uk>
2013-12-08 14:08:13 +0100

    add home.html

--- chambre/urls.py
+++ chambre/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic import TemplateView
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
+    url(r'^$', TemplateView.as_view(template_name="home.html"), name='home'),
     url(r'^pleniere/', include('pleniere.urls')),
     url(r'^deputy/', include('deputy.urls')),
 
--- pleniere/urls.py
+++ pleniere/urls.py
@@ -6,7 +6,7 @@ from pleniere.models import Pleniere
 urlpatterns = patterns('',
   url(r'^$', ListView.as_view(model=Pleniere,
       template_name='pleniere/index.html',
-      context_object_name='plenieres')),
+      context_object_name='plenieres'), name='pleniere-list'),
   url(r'^(?P<pk>\d+)/$', DetailView.as_view(model=Pleniere,
       template_name='pleniere/pleniere.html'),
       name='pleniere'),
