b874c60a6cb5bbe24b1dd5458fec44bb1fdae16c
Guillaume Desmottes <guillaume.desmottes@collabora.co.uk>
2014-01-27 11:09:08 +0100

    add todo_videos.py

---
+++ videos_tools/management/commands/todo_videos.py
@@ -0,0 +1,35 @@
+from django.core.management.base import BaseCommand
+from optparse import make_option
+
+from videos_tools.models import Video
+
+class Command(BaseCommand):
+    option_list = BaseCommand.option_list + (
+        make_option('--mms',
+          action='store_true',
+          dest='mms',
+          default=False,
+          help='Generate list of MMS stream'),
+        make_option('--mp4',
+          action='store_true',
+          dest='mp4',
+          default=False,
+          help='Generate list of MP4 videos'),
+        make_option('--webm',
+          action='store_true',
+          dest='webm',
+          default=False,
+          help='Generate list of WEBM videos'),
+    )
+
+    def handle(self, *args, **options):
+        for video in Video.objects.all():
+            if options['mms']:
+                if not video.wmv_is_ok():
+                    print video.pleniere.stream
+            elif options['mp4']:
+                if video.wmv_is_ok() and not video.mp4_is_ok():
+                    print "%s %s" % (video.pleniere.getWmvStream(), video.pleniere.getMp4Stream())
+            elif options['webm']:
+                if video.wmv_is_ok() and not video.webm_is_ok():
+                    print "%s %s" % (video.pleniere.getWmvStream(), video.pleniere.getWebmStream())
