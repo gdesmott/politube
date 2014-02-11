0fe0d5c61c6da6f7e65111430dd823342abe3d21
Guillaume Desmottes <guillaume.desmottes@collabora.co.uk>
2014-02-11 19:22:44 +0100

    rename plenary urls

--- plenary/urls.py
+++ plenary/urls.py
@@ -6,8 +6,8 @@ from plenary.models import Plenary
 urlpatterns = patterns('',
   url(r'^$', ListView.as_view(model=Plenary,
       template_name='plenary/index.html',
-      context_object_name='plenieres'), name='pleniere-list'),
+      context_object_name='plenaries'), name='plenary-list'),
   url(r'^(?P<pk>\d+)/$', DetailView.as_view(model=Plenary,
       template_name='plenary/plenary.html'),
-      name='pleniere'),
+      name='plenary'),
 )
