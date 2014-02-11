0658e538fa7745768e0c5d752c094bd7b430b031
Guillaume Desmottes <guillaume.desmottes@collabora.co.uk>
2014-02-11 19:17:29 +0100

    move plenary templats to plenary/

--- plenary/urls.py
+++ plenary/urls.py
@@ -5,9 +5,9 @@ from plenary.models import Plenary
 
 urlpatterns = patterns('',
   url(r'^$', ListView.as_view(model=Plenary,
-      template_name='pleniere/index.html',
+      template_name='plenary/index.html',
       context_object_name='plenieres'), name='pleniere-list'),
   url(r'^(?P<pk>\d+)/$', DetailView.as_view(model=Plenary,
-      template_name='pleniere/pleniere.html'),
+      template_name='plenary/plenary.html'),
       name='pleniere'),
 )
